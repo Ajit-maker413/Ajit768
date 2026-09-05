7f40a27edb924fddb5bdb4ec8181c673a31b0b72
Ajit-maker413 <25mcmc39@uohyd.ac.in>
2026-09-05 11:02:57 +0530

    Time: 1934 ms (5%), Space: 488 MB (5.46%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        int j=0;
         int i=0;
+        int j=0;
         int count=0;
-       string ss;
-       while(i<s.size())
-       {
-       // ss+=s[j];
-        string temp=s.substr(i,j-i+1);
-        string rev=temp;
-        reverse(rev.begin(),rev.end());
-        if(rev== temp)
+        while(i<s.size())
         {
-            count++;
+            string temp=s.substr(i,j-i+1);
+            string rev=temp;
+            reverse(rev.begin(),rev.end());
+            if(rev==temp)
+            {
+                count++;
+            }
+            if(j==s.size()-1)
+            {
+                
+                j=i+1;
+                i++;
+            }
+            else
+            {
+                j++;
+            }
         }
-
-         if(j==s.size()-1)
-         {
-            j=i+1;
-            i++;
-            //ss=ss.substr(i,j-1+1);
-         }
-         else
-         {
-            j++;
-         }
-
-       }
-       return count;
+        return count;
         
     }
 };
